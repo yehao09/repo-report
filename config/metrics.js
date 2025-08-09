202009d1f68c0e3a65b9cf0fee6382d54dc53a18
Hao Ye <33074691+yehao09@users.noreply.github.com>
2025-08-08 21:05:40 -0400

    Commit changes

--- config/metrics.js
+++ config/metrics.js
@@ -82,6 +82,11 @@ module.exports = {
 		extract: (item) => !!item.isSecurityPolicyEnabled,
 		permissions: ['ADMIN', 'MAINTAIN', 'WRITE'],
 	},
+	PrivateVulnerabilityReportingEnabled: {
+		compare: (item, config) => !!config === item.isPrivateVulnerabilityReportingEnabled,
+		extract: (item) => !!item.isPrivateVulnerabilityReportingEnabled,
+		permissions: ['ADMIN', 'MAINTAIN'],
+	},
 	License: {
 		compare: (item, config) => config?.includes(item.licenseInfo?.name || null),
 		extract: (item) => item.licenseInfo?.name || empty,
